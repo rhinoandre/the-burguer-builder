90dc689b92f7a5db980798b93158e7673da2a5ce
Henrique de O. Fadoni <hofadoni@users.noreply.github.com>
2018-11-29 18:21:29 -0200

    Starting the Burger Component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 
 import Aux from '../../hoc/Auxi';
+import Burger from '../../components/Burger/Burger';
 
-class BurgerBuilder extends Component {
-    
-    
+class BurgerBuilder extends Component {    
     render () {
         return (
             <Aux>
-                <div>Burger</div>
+                <Burger />
                 <div>Build Control</div>
             </Aux>
         );
