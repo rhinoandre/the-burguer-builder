92ad97e01eba5b9aec18eb72228cdd8fa23c7e86
Henrique de O. Fadoni <38730725+HenriqueFadoni@users.noreply.github.com>
2018-12-10 22:21:31 -0200

    Changing the Folder Structure

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import PropType from 'prop-types';
 
-import Aux from '../../hoc/Auxi';
+import Aux from '../Auxi/Auxi';
 import classes from './Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
