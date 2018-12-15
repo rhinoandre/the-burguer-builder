fc23aca16919f81c8e072f1e74eb62ea85e4ebfd
Henrique de O. Fadoni <38730725+HenriqueFadoni@users.noreply.github.com>
2018-12-15 16:55:04 -0200

    Sending a POST Request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../aixos-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -77,7 +78,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You Continue!');
+        //alert('You Continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Henrique Fadoni',
+                address: {
+                    street: 'tersat',
+                    zipCode: '1244141',
+                    country: 'Brazil'
+                },
+                email: 'test@testing.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render () {
