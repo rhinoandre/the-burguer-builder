4b947ef72c527c5df6c94008c0ef99828a982216
Henrique Fadoni <henriquefadonii@gmail.com>
2019-01-11 12:55:13 -0200

    Connecting ContactData Container & Actions

---
+++ src/store/actions/order.js
@@ -0,0 +1,30 @@
+import * as actionType from '../actions/actionaTypes';
+import axios from 'axios';
+
+export const purchaseBurgerSuccess = (id, orderData) => {
+    return {
+        type: actionType.PURCHASE_BURGER_SUCCESS,
+        orderId: id,
+        orderData: orderData
+    };
+};
+
+export const purchaseBurgerFail = error => {
+    return {
+        type: actionType.purchaseBurgerFail,
+        error: error
+    };
+};
+
+export const purchaseBurgerStart = orderData => {
+    return dispatch => {
+        axios.post('/orders.json', orderData)
+            .then(response => {
+                console.log(response.data);
+                dispatch( purchaseBurgerSuccess(response.data, orderData) );
+            })
+            .catch(error => {
+                dispatch( purchaseBurgerFail(error) );
+            });
+    }
+}
